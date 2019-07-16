fff7a47bd477cdfd60e1778d51b5beb021dae827
walker <qinmu911@163.com>
2019-07-16 17:50:25 +0800

    first commit

---
+++ src/main/java/com/walker/redis/RedisInActionApplication.java
@@ -0,0 +1,13 @@
+package com.walker.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisInActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisInActionApplication.class, args);
+    }
+
+}
