d2c4cd8a587aec615d9366bebfae7794ef2c8e20
walker <qinmu911@163.com>
2019-07-29 18:10:10 +0800

    feat: update redis

--- src/main/java/com/walker/redis/publish/PublishAndSubscribe.java
+++ src/main/java/com/walker/redis/publish/PublishAndSubscribe.java
@@ -1,8 +1,8 @@
 package com.walker.redis.publish;
 
 import io.lettuce.core.RedisClient;
-import io.lettuce.core.api.StatefulRedisConnection;
-import io.lettuce.core.api.sync.RedisStringCommands;
+import io.lettuce.core.pubsub.RedisPubSubListener;
+import io.lettuce.core.pubsub.api.sync.RedisPubSubCommands;
 
 /**
  * @author walker
@@ -12,9 +12,46 @@ public class PublishAndSubscribe {
 
     public static void main(String[] args) {
         RedisClient client = RedisClient.create("redis://localhost");
-        StatefulRedisConnection<String, String> connection = client.connect();
-        RedisStringCommands<String, String> sync = connection.sync();
-        sync.append("test", "1");
-        System.out.println(sync.get("test"));
+        RedisPubSubCommands<String, String> connection = client.connectPubSub().sync();
+        connection.getStatefulConnection().addListener(new RedisPubSubListener<String, String>() {
+            @Override
+            public void message(String s, String s2) {
+
+            }
+
+            @Override
+            public void message(String s, String k1, String s2) {
+
+            }
+
+            @Override
+            public void subscribed(String s, long l) {
+
+            }
+
+            @Override
+            public void psubscribed(String s, long l) {
+
+            }
+
+            @Override
+            public void unsubscribed(String s, long l) {
+                System.out.println(s);
+            }
+
+            @Override
+            public void punsubscribed(String s, long l) {
+
+            }
+        });
+        connection.subscribe("channel");
+    }
+
+    class Publisher implements Runnable {
+
+        @Override
+        public void run() {
+
+        }
     }
 }
