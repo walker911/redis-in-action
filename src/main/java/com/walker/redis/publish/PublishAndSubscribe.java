51702e44bd3c9e7b02e520b9706130461a40677a
walker <qinmu911@163.com>
2019-07-30 17:21:16 +0800

    feat: update redis

--- src/main/java/com/walker/redis/publish/PublishAndSubscribe.java
+++ src/main/java/com/walker/redis/publish/PublishAndSubscribe.java
@@ -4,6 +4,8 @@ import io.lettuce.core.RedisClient;
 import io.lettuce.core.pubsub.RedisPubSubListener;
 import io.lettuce.core.pubsub.api.sync.RedisPubSubCommands;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author walker
  * @date 2019/7/26
@@ -11,47 +13,57 @@ import io.lettuce.core.pubsub.api.sync.RedisPubSubCommands;
 public class PublishAndSubscribe {
 
     public static void main(String[] args) {
+        Thread thread = new Thread(new Publisher());
+        thread.start();
+
         RedisClient client = RedisClient.create("redis://localhost");
         RedisPubSubCommands<String, String> connection = client.connectPubSub().sync();
         connection.getStatefulConnection().addListener(new RedisPubSubListener<String, String>() {
             @Override
             public void message(String s, String s2) {
-
+                System.out.println("message|" + s + "|" + s2);
             }
 
             @Override
             public void message(String s, String k1, String s2) {
-
             }
 
             @Override
             public void subscribed(String s, long l) {
-
+                System.out.println("s|" + s + "|" + l);
             }
 
             @Override
             public void psubscribed(String s, long l) {
-
             }
 
             @Override
             public void unsubscribed(String s, long l) {
-                System.out.println(s);
+                System.out.println("un" + s + "|" + l);
             }
 
             @Override
             public void punsubscribed(String s, long l) {
-
             }
         });
         connection.subscribe("channel");
     }
 
-    class Publisher implements Runnable {
+    static class Publisher implements Runnable {
 
         @Override
         public void run() {
+            RedisClient client = RedisClient.create("redis://localhost");
+            RedisPubSubCommands<String, String> connection = client.connectPubSub().sync();
 
+            for (int i = 0; i < 10; i++) {
+                connection.publish("channel", String.valueOf(i));
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
--- src/test/java/com/walker/redis/RedisInActionApplicationTests.java
+++ src/test/java/com/walker/redis/RedisInActionApplicationTests.java
@@ -4,8 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,4 +25,80 @@ public class RedisInActionApplicationTests {
         System.out.println(result);
     }
 
+    /**
+     * 测试没有事务
+     *
+     * @throws InterruptedException
+     */
+    @Test
+    public void noTransTest() throws InterruptedException {
+        for (int i = 0; i < 3; i++) {
+            Thread thread = new Thread(() -> {
+                try {
+                    noTrans();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+            thread.start();
+            TimeUnit.MILLISECONDS.sleep(500);
+        }
+    }
+
+    private void noTrans() throws InterruptedException {
+        System.out.println(redisTemplate.opsForValue().increment("notrans:"));
+        TimeUnit.MILLISECONDS.sleep(100);
+        redisTemplate.opsForValue().increment("notrans:", -1);
+    }
+
+    /**
+     * 测试有事务
+     *
+     * @throws InterruptedException
+     */
+    @Test
+    public void transTest() throws InterruptedException {
+        for (int i = 0; i < 3; i++) {
+            Thread thread = new Thread(() -> {
+                try {
+                    trans();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+            thread.start();
+            TimeUnit.MILLISECONDS.sleep(500);
+        }
+    }
+
+    private void trans() throws InterruptedException {
+        // 开启事务
+        redisTemplate.setEnableTransactionSupport(true);
+
+        redisTemplate.multi();
+        redisTemplate.opsForValue().increment("trans:");
+        TimeUnit.MILLISECONDS.sleep(100);
+        redisTemplate.opsForValue().increment("trans:", -1);
+        System.out.println(redisTemplate.exec().get(0));
+    }
+
+    @SuppressWarnings("unchecked")
+    private void sessionCallback() {
+        SessionCallback<Object> callback = new SessionCallback<Object>() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                operations.multi();
+                operations.opsForValue().increment("trans:");
+                try {
+                    TimeUnit.MILLISECONDS.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                redisTemplate.opsForValue().increment("trans:", -1);
+                return operations.exec();
+            }
+        };
+
+        System.out.println(redisTemplate.execute(callback));
+    }
 }
